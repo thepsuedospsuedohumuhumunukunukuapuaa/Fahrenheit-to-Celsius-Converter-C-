2198bf6a464d18dfb57d2e9c2108180ef3a091f0
IHideMidNight <IHideMidNight@58FDXT2>
2023-11-07 15:50:34 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Fahrenheit to Celsius Conversion: ";
+	double fahrenheit;
+	cin >> fahrenheit;
+	cout << fahrenheit << " Degrees fahrenheit is equal to exactly " << fahrenheit / 32 << " degrees celsius";
+	return 0;
+}
